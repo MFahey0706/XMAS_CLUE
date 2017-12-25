4f6e85cba615e3ef58a838340f43106b6d031b02
BTOwlregard <fahey.matt@gmail.com>
2017-12-24 23:52:17 -0600

    add conditional removal of existing CLUE file

--- run_me_plz.sh
+++ run_me_plz.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #Oooeeeaugh ooooh, spooky Christmas techno-mystery
+if [ -e "CLUE.txt" ]
+then rm CLUE.txt
+fi
 if [ -e "super_secret_xmas_clue.txt" ]
 then cat super_secret_xmas_clue.txt | awk '/([Ba].xM[by]a[Jesu]5)|^T[5-9]/' | cut -c 10 >> CLUE.txt
 else echo "Input file not found. Plz acquire and save in current working directory. Btw, Stiphanie sends her regards, but she sent them to Sam by mistake. Oops."
