7e4170f11c7a323e3ed77ff5137c0ee5f6025d5c
BTOwlregard <fahey.matt@gmail.com>
2017-12-24 23:26:50 -0600

    Update run_me_plz.sh to pipe to file
    Piping to file bc clue may be too long for window

--- run_me_plz.sh
+++ run_me_plz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Oooeeeaugh ooooh, spooky Christmas techno-mystery
 if [ -e "super_secret_xmas_clue.txt" ]
-then cat super_secret_xmas_clue.txt | awk '/([Ba].xM[by]a[Jesu]5)|^T[5-9]/' | cut -c 10
+then cat super_secret_xmas_clue.txt | awk '/([Ba].xM[by]a[Jesu]5)|^T[5-9]/' | cut -c 10 >> CLUE.txt
 else echo "Input file not found. Plz acquire and save in current working directory. Btw, Stiphanie sends her regards, but she sent them to Sam by mistake. Oops."
 fi
 
